601b923c1edfa6bb483e27c65f2f8dfc300fab8d
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-21 14:58:12 +0300

    Refactor MRU cache implementation to use `popitem(last=True)`

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -12,19 +12,22 @@ class MRUCache(BaseCaching):
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """method for adding an item in the cache"""
+        """Adds an item in the cache.
+        """
         if key is None or item is None:
             return
-        if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-            discarded_key, _ = self.cache_data.popitem(False)
-            print("DISCARD: {}".format(discarded_key))
-        if key in self.cache_data:
-            self.cache_data.move_to_end(key)
-        self.cache_data[key] = item
+        if key not in self.cache_data:
+            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                discarded_key, _ = self.cache_data.popitem(False)
+                print("DISCARD:", discarded_key)
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=False)
+        else:
+            self.cache_data[key] = item
 
     def get(self, key):
-        """method for accessing values of a key"""
-        if key is None or key not in self.cache_data:
-            return None
-        self.cache_data.move_to_end(key)
-        return self.cache_data[key]
+        """Retrieves an item by key.
+        """
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key, None)
