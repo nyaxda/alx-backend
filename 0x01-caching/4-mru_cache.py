0f822e6c64cbe414a1073ae9a3f2c30a08842343
Daniel Nyakundi <nyaxda@gmail.com>
2024-08-21 13:25:32 +0300

    Added File 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" MRU Caching
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class MRUCache(BaseCaching):
+    """MRU caching class inheriting from BaseCaching"""
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """method for adding an item in the cache"""
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            discarded_key, _ = self.cache_data.popitem()
+            print("DISCARD: {}".format(discarded_key))
+
+    def get(self, key):
+        """method for accessing values of a key"""
+        if key is None or key not in self.cache_data:
+            return None
+        self.cache_data.move_to_end(key)
+        return self.cache_data[key]
